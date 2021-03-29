d2706c60dc877c4e624b4381fe04d37851f95c69
zzangs33 <zzangs33@forcs.com>
2021-03-29 14:32:10 +0900

    Initial commit

---
+++ src/com/coding/march/fifth/ThisWeek.java
@@ -0,0 +1,15 @@
+package com.coding.march.fifth;
+
+/**
+ * @link https://programmers.co.kr/learn/courses/30/parts/12077
+ */
+public interface ThisWeek {
+
+    String theNotFinished(String[] participant, String[] completion);
+
+    boolean telNumList(String[] phone_book);
+
+    int camouflage(String[][] clothes);
+
+     int[] bestAlbum(String[] genres, int[] plays);
+}
---
+++ src/com/coding/march/fifth/Zzangs33.java
@@ -0,0 +1,27 @@
+package com.coding.march.fifth;
+
+public class Zzangs33 implements ThisWeek {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public String theNotFinished(String[] participant, String[] completion) {
+        return null;
+    }
+
+    @Override
+    public boolean telNumList(String[] phone_book) {
+        return false;
+    }
+
+    @Override
+    public int camouflage(String[][] clothes) {
+        return 0;
+    }
+
+    @Override
+    public int[] bestAlbum(String[] genres, int[] plays) {
+        return new int[0];
+    }
+}
