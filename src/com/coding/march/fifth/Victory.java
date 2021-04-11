5c1f135ac2d8e6ccc643b9a1714e946b7fdb5fe4
박아란 <koreacoolar@gmail.com>
2021-04-11 22:34:18 +0900

    2번 문제 해결 3번문제 푸는중

--- src/com/coding/march/fifth/Victory.java
+++ src/com/coding/march/fifth/Victory.java
@@ -1,11 +1,13 @@
 package com.coding.march.fifth;
 
+import javax.print.DocFlavor;
 import java.util.*;
 
 public class Victory implements Problem{
 
   public static void main(String[] args) {
-
+    Victory vic = new Victory();
+    vic.telNumList(new String[]{"123", "456", "789"});
   }
 
   @Override
@@ -31,13 +33,42 @@ public class Victory implements Problem{
   @Override
   public boolean telNumList(String[] phone_book) {
     boolean answer = true;
+    HashSet<String> set = new HashSet<>(Arrays.asList(phone_book));
 
+    for(String phone : phone_book) {
+      StringBuffer buffer = new StringBuffer();
+
+      set.remove(phone);
+      for(int i=0; i< phone.length(); i++) {
+        buffer.append(phone.charAt(i));
+
+        if(set.contains(buffer.toString())) {
+          return false;
+        };
+      }
+      set.add(phone);
+    }
     return answer;
+
+
   }
 
   @Override
   public int camouflage(String[][] clothes) {
-    return 0;
+    HashMap<String, List<String>> map = new HashMap<>();
+
+    for(String[] StrArr: clothes) {
+      List<String> list = map.get(StrArr[1]);
+      if(list != null) {
+        List<String> newList = new ArrayList<>();
+        newList.add(StrArr[0]);
+        map.put(StrArr[1], newList);
+      } else {
+        list.add(StrArr[0]);
+      }
+    }
+    //TODO 경우의 수 계산
+      return 0;
   }
 
   @Override
