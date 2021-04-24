811ec0d85967045e221ff7e38f3ac2e82a8bc570
Pyeon Huijin <zzangs33@hanmail.net>
2021-04-24 21:45:32 +0900

    저번주랑 이번주꺼 올렸습니다 ㅜㅜ 늦게올려서 ㅈㅅ..

---
+++ src/com/coding/april/forth/Kms27420.java
@@ -0,0 +1,18 @@
+package com.coding.april.forth;
+
+public class Kms27420 implements Problem{
+    @Override
+    public int[] virtualTest(int[] answers) {
+        return new int[0];
+    }
+
+    @Override
+    public int findPrimeNumber(String numbers) {
+        return 0;
+    }
+
+    @Override
+    public int[] carpet(int brown, int yellow) {
+        return new int[0];
+    }
+}
