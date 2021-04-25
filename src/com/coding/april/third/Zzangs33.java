56080d5203a65b5496ae816e759c59e4bb8355ac
Pyeon Huijin <zzangs33@hanmail.net>
2021-04-25 13:32:38 +0900

    정렬 두번째 문제 해결

--- src/com/coding/april/third/Zzangs33.java
+++ src/com/coding/april/third/Zzangs33.java
@@ -1,7 +1,6 @@
 package com.coding.april.third;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -9,7 +8,9 @@ public class Zzangs33 implements Sorting {
     public static void main(String[] args) {
         Zzangs33 zzangs33 = new Zzangs33();
 
-        System.out.println(Arrays.toString(zzangs33.kthNumber(new int[]{1, 5, 2, 6, 3, 7, 4}, new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}})));
+//        System.out.println(Arrays.toString(zzangs33.kthNumber(new int[]{1, 5, 2, 6, 3, 7, 4}, new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}})));
+//        System.out.println(zzangs33.theBiggestNumber(new int[]{6, 10, 2}));
+//        System.out.println(zzangs33.theBiggestNumber(new int[]{3, 30, 34, 5, 9}));
 
     }
 
@@ -35,7 +36,20 @@ public class Zzangs33 implements Sorting {
 
     @Override
     public String theBiggestNumber(int[] numbers) {
-        return null;
+        List<Integer> list = new ArrayList<>();
+
+        for (int num : numbers) list.add(num);
+        list.sort((a, b) -> {
+            String s1 = a + Integer.toString(b);
+            String s2 = b + Integer.toString(a);
+
+            return -s1.compareTo(s2);
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : list) sb.append(num);
+
+        return list.get(0) == 0 ? "0" : sb.toString();
     }
 
     @Override
