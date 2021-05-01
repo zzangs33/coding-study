4647a580f9d49fe73b1fd7415544b58b4973e600
Pyeon Huijin <zzangs33@hanmail.net>
2021-05-01 20:17:51 +0900

    4월 마지막 주 문제 추가

---
+++ src/com/coding/april/fifth/Greedy.java
@@ -0,0 +1,18 @@
+package com.coding.april.fifth;
+
+/**
+ * @link https://programmers.co.kr/learn/courses/30/parts/12244
+ */
+public interface Greedy {
+    int gymSuit(int n, int[] lost, int[] reserve);
+
+    int joyStick(String name);
+
+    String makeABigNumber(String number, int k);
+
+    int lifeboat(int[] people, int limit);
+
+    int linkingIslands(int n, int[][] costs);
+
+    int speedTrap(int[][] routes);
+}
