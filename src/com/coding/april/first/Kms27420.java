6a200995ae606f9ebcbb7f5727ca2f9173145c97
kms <kms27420@forcs.com>
2021-05-05 18:32:17 +0900

    Solved April stockValue problem

--- src/com/coding/april/first/Kms27420.java
+++ src/com/coding/april/first/Kms27420.java
@@ -5,15 +5,15 @@ import java.util.LinkedList;
 public class Kms27420 implements QueueNStack {
     public static void main(String[] args) {
         Kms27420 instance = new Kms27420();
-        instance.exe("truckThroughBridge");
+//        instance.exe("truckThroughBridge");
 //        instance.exe("stockValue");
-//        instance.exe("developModule");
+        instance.exe("developModule");
 //        instance.exe("printer");
     }
 
     @Override
     public int truckThroughBridge(int bridge_length, int weight, int[] truck_weights) {
-        LinkedList<Info> queue = new LinkedList<>();
+        LinkedList<TruckInfo> queue = new LinkedList<>();
         int i = 0, passCnt = 0, tick = 0, totalWeight = 0, item;
         while (passCnt < truck_weights.length) {
             if (!queue.isEmpty() && queue.get(0).breakPoint == tick) {
@@ -23,7 +23,7 @@ public class Kms27420 implements QueueNStack {
             if (i < truck_weights.length) {
                 item = truck_weights[i];
                 if (item + totalWeight <= weight) {
-                    queue.add(new Info(item, tick + bridge_length));
+                    queue.add(new TruckInfo(item, tick + bridge_length));
                     totalWeight += item;
                     i += 1;
                 }
@@ -33,11 +33,11 @@ public class Kms27420 implements QueueNStack {
         return tick;
     }
 
-    private static final class Info {
+    private static final class TruckInfo {
         private final int weight;
         private final int breakPoint;
 
-        private Info(int weight, int breakPoint) {
+        private TruckInfo(int weight, int breakPoint) {
             this.weight = weight;
             this.breakPoint = breakPoint;
         }
@@ -45,7 +45,26 @@ public class Kms27420 implements QueueNStack {
 
     @Override
     public int[] stockValue(int[] prices) {
-        return new int[0];
+        return this.stockValue(prices, 0);
+    }
+
+    private int[] stockValue(int[] prices, int idx) {
+        if (idx >= prices.length - 1) return new int[prices.length];
+        int[] result = this.stockValue(prices, idx + 1);
+        if (prices[idx] > prices[idx + 1]) {
+            result[idx] = 1;
+            return result;
+        }
+        int temp = -1;
+        for (int i = idx + 1 + result[idx + 1]; i < prices.length; i++)
+            if (prices[idx] > prices[i]) {
+                temp = i - idx;
+                break;
+            }
+        if (temp == -1)
+            temp = prices.length - idx - 1;
+        result[idx] = temp;
+        return result;
     }
 
     @Override
