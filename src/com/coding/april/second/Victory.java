7e2a29c4c0d0f005c3fe06b101c6c1678f433176
Victory Park <victory@forcs.com>
2021-05-12 11:17:33 +0900

    Solved hotter question

--- src/com/coding/april/second/Victory.java
+++ src/com/coding/april/second/Victory.java
@@ -1,9 +1,36 @@
 package com.coding.april.second;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
 public class Victory implements Heap {
+
+    int fi;
+    public static void main(String[] args) {
+        Victory vic = new Victory();
+        vic.exe("hotter");
+    }
     @Override
     public int hotter(int[] scoville, int K) {
-        return 0;
+        Queue<Integer> hotQ = new PriorityQueue<>(Arrays.stream(scoville).boxed().collect(Collectors.toList()));
+        int cnt = 0;
+        int notHot;
+        while(!hotQ.isEmpty()) {
+            notHot = hotQ.poll();
+            if(notHot >= K) {
+                return cnt;
+            } else {
+                if(hotQ.isEmpty()) return -1;
+                int next = hotQ.poll();
+                hotQ.offer(notHot + (next*2));
+                cnt ++;
+            }
+        }
+
+        return -1;
     }
 
     @Override
