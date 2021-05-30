88ec56177dde46f5b7fdf351ab77629459c34968
박아란 <koreacoolar@gmail.com>
2021-05-30 23:01:01 +0900

    tried to solve Greedy first question

--- src/com/coding/april/fifth/Victory.java
+++ src/com/coding/april/fifth/Victory.java
@@ -1,9 +1,45 @@
 package com.coding.april.fifth;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Victory implements Greedy {
+
+    public static void main(String[] args) {
+        Victory vic = new Victory();
+        vic.exe("gymSuit");
+    }
     @Override
     public int gymSuit(int n, int[] lost, int[] reserve) {
-        return 0;
+        List<Integer> reserveList = new ArrayList<>();
+        List<Integer> lostList = new ArrayList<>();
+
+        for (int no : reserve) reserveList.add(no);
+        for (int no : lost) {
+            if(reserveList.contains(no)) reserveList.remove((Integer) no);
+            else lostList.add(no);
+        }
+        System.out.println("1st lostList :" + lostList);
+        System.out.println("1st reserveList :" + reserveList);
+        List<Boolean> canDoPhy = new ArrayList<>();
+
+        boolean alreadyChange = false;
+        for(Integer no : lostList) {
+            if(reserveList.size() == 0) break;
+            if(reserveList.contains(no-1)) {
+                canDoPhy.add(true);
+                reserveList.remove(reserveList.indexOf(no-1));
+            }
+            if(reserveList.size() >0 && reserveList.contains(no+1)) {
+                canDoPhy.add(true);
+                reserveList.remove(reserveList.indexOf(no+1));
+            }
+            else canDoPhy.add(false);
+            System.out.println("reserveList :" + reserveList);
+        }
+
+
+        return (int) (n - canDoPhy.stream().filter(is-> !is).findAny().stream().count());
     }
 
     @Override
