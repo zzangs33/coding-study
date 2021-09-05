0423345f0a10d0ca54d381778b02d97619775072
DESKTOP-RK99VIH\kms27 <kms27420@gmail.com>
2021-09-05 15:37:08 +0900

    [권민성/4월/5주차/체육복] 풀이완료

--- src/com/coding/april/fifth/Kms27420.java
+++ src/com/coding/april/fifth/Kms27420.java
@@ -1,19 +1,44 @@
 package com.coding.april.fifth;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Kms27420 implements Greedy {
     public static void main(String[] args) {
         Kms27420 instance = new Kms27420();
         instance.exe("gymSuit");
-        instance.exe("joyStick");
-        instance.exe("makeABigNumber");
-        instance.exe("lifeboat");
-        instance.exe("linkingIslands");
-        instance.exe("speedTrap");
+//        instance.exe("joyStick");
+//        instance.exe("makeABigNumber");
+//        instance.exe("lifeboat");
+//        instance.exe("linkingIslands");
+//        instance.exe("speedTrap");
     }
 
     @Override
     public int gymSuit(int n, int[] lost, int[] reserve) {
-        return 0;
+        Set<Integer> reserveSet = Arrays.stream(reserve)
+                .boxed()
+                .collect(Collectors.toSet());
+        List<Integer> lostList = Arrays.stream(lost)
+                .boxed()
+                .filter(l -> !reserveSet.remove(l))
+                .sorted()
+                .collect(Collectors.toList());
+
+        int lostCnt = lostList.size();
+
+        for (Integer l : lostList) {
+            for (int i : new int[]{l - 1, l + 1}) {
+                if (reserveSet.remove(i)) {
+                    lostCnt -= 1;
+                    break;
+                }
+            }
+        }
+
+        return n - lostCnt;
     }
 
     @Override
