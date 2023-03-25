8d6cf4c8d639c6871a46b7d2323897efc767bc1a
DESKTOP-RK99VIH\kms27 <kms27420@gmail.com>
2023-03-25 21:01:22 +0900

    [2023/4주차/카카오기출] 인터페이스 & 테스트케이스

---
+++ src/main/java/com/coding/y2023/march/forth/Kakao2023.java
@@ -0,0 +1,20 @@
+package com.coding.y2023.march.forth;
+
+/**
+ * @link "https://school.programmers.co.kr/learn/challenges?order=recent&partIds=37527"
+ */
+public interface Kakao2023 {
+    int[] privacyInfo(String today, String[] terms, String[] privacies);
+
+    long delivery(int cap, int n, int[] deliveries, int[] pickups);
+
+    int[] emoticon(int[][] users, int[] emoticons);
+
+    int[] binaryTree(long[] numbers);
+
+    String[] mergeTable(String[] commands);
+
+    String escape(int n, int m, int x, int y, int r, int c, int k);
+
+    int[] oneTwoThree(int[][] edges, int[] target);
+}
