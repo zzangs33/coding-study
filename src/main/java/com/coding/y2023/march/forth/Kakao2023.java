d8463474795844eb850239d1c968f1a5053096da
DESKTOP-RK99VIH\kms27 <kms27420@gmail.com>
2023-03-26 12:29:23 +0900

    Kakao2023.java TestCase annotation 적용

--- src/main/java/com/coding/y2023/march/forth/Kakao2023.java
+++ src/main/java/com/coding/y2023/march/forth/Kakao2023.java
@@ -1,10 +1,15 @@
 package com.coding.y2023.march.forth;
 
+import com.coding.testcase.TestCase;
+
+import java.text.ParseException;
+
 /**
  * @link "https://school.programmers.co.kr/learn/challenges?order=recent&partIds=37527"
  */
+@TestCase("/com/coding/y2023/march/forth/kakao2023.json")
 public interface Kakao2023 {
-    int[] privacyInfo(String today, String[] terms, String[] privacies);
+    int[] privacyInfo(String today, String[] terms, String[] privacies) throws ParseException;
 
     long delivery(int cap, int n, int[] deliveries, int[] pickups);
 
